8752e633872f83ceb0b418217fff818898eeca0f
Archisha Chatterjee <archisha.chatterjee.btech2024@sitpune.edu.in>
2025-08-20 01:18:06 +0530

    Create Exp10A.cpp

---
+++ Exp10A.cpp
@@ -0,0 +1,28 @@
+// EXP 10A : Call by value (without pointers)
+
+# include <iostream>
+using namespace std;
+
+void swap (int x, int y){
+    int temp = x;
+    x = y;
+    y = temp;   
+    return ;
+    }
+
+int main(){
+    int a = 10;
+    int b = 20;
+    cout <<  a << endl;
+    swap(a, b);
+    cout << b << endl;
+    return 0;
+
+}
+
+/*
+OUTPUT :
+10
+10
+
+*/
